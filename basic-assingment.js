9b8877ae64f07e584f1d2243eb65e4d461032580
unknown <mohiuddinshanto1135@gmail.com>
2021-08-09 17:56:13 +0600

    done

--- basic-assingment.js
+++ basic-assingment.js
@@ -1,27 +1,125 @@
-//problem number 1
-function seerToMon(seer) {
-    const mon = seer / 40;
-    return mon;
-} let mons = seerToMon(120);
-console.log(mons)
 
+/*======================
+  1.  problem solve -start 
+  ======================*/
 
-//problem number 2
-
-function totalSales(shirtQuantity,pantsQuantity,shoesQuantity) {
-    const shirtPrices = 100;
-    const pantsPrices= 200;
-    const shoesPrices = 500;
-    
-    const shirtPriceQuantity = shirtQuantity * shirtPrices;
-    const pantsPriceQuantity = pantsQuantity * pantsPrices;
-    const shoesPriceQuantity = shoesQuantity * shoesPrices;
-
-    let sum = shirtPriceQuantity + pantsPriceQuantity + shoesPriceQuantity ;
-    return sum;
+  function seerToMon(seer){
+    if (seer < 0) {
+        console.log("please Enter  positive value ")
+    }else {
+        let mon = seer / 40;
+        return mon;
+    }
 }
-    let totalSaless = totalSales(5,5,1);
-    console.log(totalSaless);
 
-    //problem number 3
+let result = seerToMon(40);
+console.log(result)
 
+/*======================
+  1.  problem solve -end  
+  ======================*/
+
+
+
+/*======================
+  2.  problem solve -start 
+  ======================*/
+
+function totalSales(shirtQuantity, pantQuantity, shoeQuantity){
+    let pershirt = 100;
+    let perPant = 200;
+    let perShoe = 500;
+
+    if (shirtQuantity <= 0 || pantQuantity <= 0 || shoeQuantity <= 0 ) {
+        console.log("Please Enter your valid quantity ")
+    } else {
+        let shirtCost  = pershirt * shirtQuantity
+        let pantCost  = perPant * pantQuantity
+        let shoeCost  = perShoe *  shoeQuantity
+        let totalCost = shirtCost + pantCost + shoeCost;
+    
+        return totalCost;
+    }
+    
+
+}
+
+let totalSalesResult = totalSales(1,2,1);
+console.log(totalSalesResult)
+
+/*======================
+  2.  problem solve -end 
+  ======================*/
+
+ 
+  /*======================
+  3.  problem solve -start 
+  ======================*/
+
+function deliveryCost(tshirtQuantity){
+    let firstHundredTshirtCost = 100;
+    let secondtHundredTshirtCost = 80;
+    let thirdHundredTshirtCost = 50;
+
+    if (tshirtQuantity <= 0 ) {
+        console.log("Please enter you valid quntity")
+    }else{
+        if (tshirtQuantity <= 100) {
+            return  tshirtQuantity * firstHundredTshirtCost;
+        }
+        else if (tshirtQuantity <= 200) {
+           let secondRestTshirt = tshirtQuantity - 100;
+           let firstShirtCost = firstHundredTshirtCost * 100;
+           let  secondtHundredTshirtCostTotal = firstShirtCost +   secondtHundredTshirtCost * secondRestTshirt;
+           return secondtHundredTshirtCostTotal;        
+        }
+        
+        else if (tshirtQuantity <= 300) {
+            let thirdRestTshirt = tshirtQuantity - 200;
+            let firstShirtCost = firstHundredTshirtCost * 100;
+            let secondShirtCost = secondtHundredTshirtCost * 100;
+            let  thirdtHundredTshirtCostTotal = firstShirtCost + secondShirtCost +  thirdHundredTshirtCost * thirdRestTshirt;
+            return thirdtHundredTshirtCostTotal;         
+         }
+    }
+    
+}
+
+let tshirtPriceResult = deliveryCost(101)
+console.log(tshirtPriceResult)
+
+  /*======================
+  3.  problem solve -end 
+  ======================*/
+
+
+
+  /*======================
+  4.  problem solve -start
+  ======================*/
+
+
+let freinds = ['alamin', 'kamrul', 'rocky','sanjaf'];
+function perfectFriend(arr){
+    let perfectFriendName = '';
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (element.length == 5) {
+            perfectFriendName = element;
+            break;           
+        }        
+    }
+
+    if (perfectFriendName == '') {
+        console.log("not found perfect freind ");
+    }else {
+        return perfectFriendName;
+    }   
+}
+
+
+let perfectFriendResult  = perfectFriend(freinds);
+console.log(perfectFriendResult)
+  /*======================
+  4.  problem solve -end
+  ======================*/
---
+++ practies.js
@@ -0,0 +1,39 @@
+// question-1
+var number = 10;
+var name = 'shanto'
+var answere = true;
+
+// question -2
+let numbers = 10;
+const numbers2 = 30;
+numbers = 50;
+console.log(numbers,numbers2);
+
+// question - 3
+
+var price = 10;
+var price2 = 20;
+var sum = price+price2;
+console.log(sum);
+
+var price = 10;
+var price2 = 20;
+var sum = price*price2;
+console.log(sum);
+
+function biyog (price,price2){
+    var sum = price-price2;
+    return sum
+} var totalSum =biyog(100,95)
+console.log(totalSum);
+
+function vag(price,price2){
+    let sum = price/price2;
+    return sum
+} let totalSums = vag(50,10)
+console.log(totalSums);
+
+var taka = 100;
+if(taka %2 == 0){
+    console.log('vagsesh thake na')
+}
